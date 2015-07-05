4ae686653d0b56daa34c468ae694ac8455d5102a
saltern <saltern@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-05 18:55:00 +0000

    Updated code matches up to WooCommerce 2.3.11. No new features added.
    git-svn-id: https://plugins.svn.wordpress.org/xo10-woocommerce-categories-widget/trunk@1192887 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/walkers/class-cat-list-walker.php
+++ includes/walkers/class-cat-list-walker.php
@@ -1,5 +1,6 @@
 <?php
 
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
@@ -13,26 +14,26 @@ class XO10_WC_Cat_List_Walker extends WC_Product_Cat_List_Walker {
   public $imgw = 42;
   public $imgh = 42;
   public $forcecss = 'no';
-  
+
 	/**
    * Adds thumbnails if required.
-   * 
+   *
 	 * @see WC_Product_Cat_List_Walker::start_el()
 	 */
 	public function start_el( &$output, $cat, $depth = 0, $args = array(), $current_object_id = 0 ) {
-				
+
     $thumbnail_id = get_woocommerce_term_meta( $cat->term_id, 'thumbnail_id', true );
 		$image = wp_get_attachment_image_src( $thumbnail_id, 'detail', false );
-    
+
     $imgsrc = '';
     if( false === $image ) {
-      $imgsrc = wc_placeholder_img_src(); 
+      $imgsrc = wc_placeholder_img_src();
     } else {
       $imgsrc = $image[0];
     }
-    
+
     $img_constraint = 'width="' . absint( $this->imgw ) . '" height="' . absint( $this->imgh ) . '"';
-    
+
     $li_styles = '';
     switch( $this->forcecss ) {
       case 'vsp10':
@@ -42,7 +43,7 @@ class XO10_WC_Cat_List_Walker extends WC_Product_Cat_List_Walker {
       default:
         break;
     }
-    
+
     $output .= '<li ' . $li_styles . ' class="cat-item cat-item-' . $cat->term_id;
 
 		if ( $args['current_category'] == $cat->term_id ) {
@@ -58,12 +59,12 @@ class XO10_WC_Cat_List_Walker extends WC_Product_Cat_List_Walker {
 		}
 
     // post count on extreme left
-    if( $args['show_count'] && $this->countpos == 'extleft' ) { 
+    if( $args['show_count'] && $this->countpos == 'extleft' ) {
       $output .=  '"><span class="count">(' . $cat->count . ')</span> <a href="' . get_term_link( (int) $cat->term_id, 'product_cat' ) . '">';
     } else {
       $output .=  '"><a href="' . get_term_link( (int) $cat->term_id, 'product_cat' ) . '">';
     }
-            
+
     if( $this->imgtxt == 'text' ) {
       $output .= '<span class="cat-name">' . __( $cat->name, 'woocommerce' ) . '</span></a>';
     } elseif ( $this->imgtxt == 'image' ) {
@@ -73,9 +74,9 @@ class XO10_WC_Cat_List_Walker extends WC_Product_Cat_List_Walker {
     } else { // iltr i.e. image left, text right
       $output .= '<img src="' . $imgsrc . '" title="' . __( $cat->name, 'woocommerce' ) . '" alt="' . __( $cat->name, 'woocommerce' ) . '" ' . $img_constraint . ' /> <span class="cat-name">' .  __( $cat->name, 'woocommerce' ) . '</span></a>';
     }
-    
+
     // post count on extreme right
-    if ( $args['show_count'] && $this->countpos == 'extright' ) { 
+    if ( $args['show_count'] && $this->countpos == 'extright' ) {
 			$output .= ' <span class="count">(' . $cat->count . ')</span>';
 		}
 
--- xo10-wc-categories-widget.php
+++ xo10-wc-categories-widget.php
@@ -3,7 +3,7 @@
  * Plugin Name: XO10 - WooCommerce Categories widget
  * Plugin URI: http://cartible.com
  * Description: Adds a widget that is able to display WooCommerce product category image thumbnails.
- * Version: 1.1
+ * Version: 1.2
  * Author: Walter Lee
  * Author URI: http://cartible.com
  * Requires at least: 3.9
@@ -17,10 +17,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-include_once( ABSPATH . 'wp-admin/includes/plugin.php' ); 
+include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
 // check that woocommerce is active
-$plugin = plugin_basename( __FILE__ );		
+$plugin = plugin_basename( __FILE__ );
 if ( !in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
   if( is_plugin_active( $plugin ) ) {
      //deactivate_plugins( $plugin );
@@ -28,14 +28,13 @@ if ( !in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', 
   }
 }
 
-
 if ( ! class_exists( 'XO10_WC_Cats_Plugin' ) ) :
 
 /**
  * Main XO10_WC_Cats_Plugin class
  */
 final class XO10_WC_Cats_Plugin {
-  
+
 	/**
 	 * @var XO10_WC_Cats_Plugin The single instance of the class
 	 */
@@ -63,7 +62,7 @@ final class XO10_WC_Cats_Plugin {
 
 		// Hooks
 		add_action( 'widgets_init', array( $this, 'include_widgets' ) );
-    
+
     // Housekeeping Hooks
     if(is_admin() ) {
       add_action( 'admin_notices',  array( $this, 'inactive_wc_alert' ) ) ;
@@ -76,16 +75,16 @@ final class XO10_WC_Cats_Plugin {
 	 * Define constants we need
 	 */
 	private function define_constants() {
-    
+
     $plugin_data = get_plugin_data( __FILE__, false, false );
 		define( 'XO10_WC_CATS_PLUGIN_OFFICIAL_NAME', $plugin_data['Name'] );
-    
+
 		define( 'XO10_WC_CATS_PLUGIN_PLUGIN_FILE', __FILE__ );
 		define( 'XO10_WC_CATS_PLUGIN_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 
-    define( 'XO10_WC_CATS_PLUGIN_MIN_VERSION_WP' , '3.9' ); 
+    define( 'XO10_WC_CATS_PLUGIN_MIN_VERSION_WP' , '3.9' );
     define( 'XO10_WC_CATS_PLUGIN_MIN_VERSION_PHP' , '5.3' );
-    define( 'XO10_WC_CATS_PLUGIN_MIN_VERSION_WC' , '2.2' );
+    define( 'XO10_WC_CATS_PLUGIN_MIN_VERSION_WC' , '2.3' );
 
     define( 'XO10_WC_CATS_PLUGIN_VERSION', $plugin_data['Version'] );
     define( 'XO10_WC_CATS_PLUGIN_DB_VERSION', '1.0' );
@@ -95,18 +94,18 @@ final class XO10_WC_Cats_Plugin {
     define( 'XO10_WC_CATS_PLUGIN_INCLUDES', XO10_WC_CATS_PLUGIN_DIR . trailingslashit( 'includes' ) );
 	}
 
-  
+
 	/**
 	 * Include required core files used in admin and on the frontend.
 	 */
 	private function includes() {
-    require_once( XO10_WC_CATS_PLUGIN_INCLUDES . 'widgets/class-categories.php' ); 
+    require_once( XO10_WC_CATS_PLUGIN_INCLUDES . 'widgets/class-categories.php' );
     include_once( XO10_WC_CATS_PLUGIN_INCLUDES . 'walkers/class-cat-list-walker.php' );
     include_once( XO10_WC_CATS_PLUGIN_DIR . '../woocommerce/includes/abstracts/abstract-wc-widget.php' );
     include_once( XO10_WC_CATS_PLUGIN_DIR . '../woocommerce/includes/walkers/class-product-cat-list-walker.php' );
 	}
 
-  
+
 	/**
 	 * Include core widgets
 	 */
@@ -114,11 +113,11 @@ final class XO10_WC_Cats_Plugin {
     register_widget( 'XO10_WC_Categories_Widget' );
 	}
 
-  
+
   // -----------------------------------------
   // Housekeeping
   // -----------------------------------------
-  
+
   /**
    * Displays error message if WooCommerce is inactive.
    */
@@ -127,7 +126,7 @@ final class XO10_WC_Cats_Plugin {
       include_once( XO10_WC_CATS_PLUGIN_INCLUDES . 'views/alert-wc-inactive.php' );
     }
   }
-  
+
   /**
    * Checks environment when plugin is activated.
    */
@@ -141,19 +140,22 @@ final class XO10_WC_Cats_Plugin {
     }
   }
 
+	/**
+   * Deletes all plugin data on uninstall.
+   */
   static function purge_data() {
     delete_option( 'widget_' . XO10_WC_Categories_Widget::WIDGET_SLUG );
   }
-  
-  
+
+
 } // end class
 
 endif;
 
-
 function xo10_wc_cats_go() {
 	return XO10_WC_Cats_Plugin::instance();
 }
+
 
 // run the code
 xo10_wc_cats_go();
